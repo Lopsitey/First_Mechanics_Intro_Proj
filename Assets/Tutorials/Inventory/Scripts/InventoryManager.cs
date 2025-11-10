a3e0af64f85e241b128e6a968f7a4a8ee65d0e73
Jake Astles <jakeastles13@gmail.com>
2025-11-10 19:59:03 +0000

    Improved understanding of Inventory Manager and Added Comments

--- Assets/Tutorials/Inventory/Scripts/InventoryManager.cs
+++ Assets/Tutorials/Inventory/Scripts/InventoryManager.cs
@@ -1,141 +1,177 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+namespace Tutorials.Inventory.Scripts
 {
-    [SerializeField] private GameObject m_InventoryPrefab;
-
-    [SerializeField] private GameObject m_EmptySlotContainer;
-    [SerializeField] private GameObject m_FilledSlotContainer;
-
-    [SerializeField] private int m_MaxSlots = 10;
-    [SerializeField] private int m_CurrentSlots = 10;
-
-    private ItemSO[] m_ItemDatabase;
-
-    private List<GameObject> m_EmptySlots;
-    private List<GameObject> m_FilledSlots;
-
-    void Start()
+    public class InventoryManager : MonoBehaviour
     {
-        //Debug Init - This should ideally be initialised elsewhere during setup.
-        Init();
-    }
+        [SerializeField] private GameObject m_InventoryPrefab;
 
-    public void Init()
-    {
-        m_ItemDatabase = Resources.LoadAll<ItemSO>("");
+        [SerializeField] private GameObject m_EmptySlotContainer;
+        [SerializeField] private GameObject m_FilledSlotContainer;
 
-        m_EmptySlots = new List<GameObject>();
-        m_FilledSlots = new List<GameObject>();
+        [SerializeField] private int m_MaxSlots = 10;
+        [SerializeField] private int m_CurrentSlots = 10;//for adding scrolling functionality
 
-        for (int i = 0; i < m_MaxSlots; i++)
+        private ItemSO[] m_ItemDatabase;
+
+        private List<GameObject> m_EmptySlots;
+        private List<GameObject> m_FilledSlots;
+
+        void Start()
         {
-            GameObject slot = Instantiate(m_InventoryPrefab, m_EmptySlotContainer.transform);
-            m_EmptySlots.Add(slot);
+            //Debug Init - This should ideally be initialised elsewhere during setup.
+            Init();
         }
-    }
 
-    public void TestAdd()
-    {
-        Add(1, 10);
-        Add(2, 1);
-    }
-
-    public void TestRemove()
-    {
-        Remove(1, 7);
-        Remove(2, 1);
-    }
-
-    public void Add(int itemID, int quantity)
-    {
-        int excess = 0;
-
-        if (m_EmptySlots.Count > 0)
+        public void Init()
         {
-            foreach (GameObject slot in m_FilledSlots)
+            m_ItemDatabase = Resources.LoadAll<ItemSO>("");
+
+            m_EmptySlots = new List<GameObject>();
+            m_FilledSlots = new List<GameObject>();
+
+            for (int i = 0; i < m_MaxSlots; i++)
             {
-                InventorySlot invSlot = slot.GetComponent<InventorySlot>();
+                GameObject slot = Instantiate(m_InventoryPrefab, m_EmptySlotContainer.transform);
+                m_EmptySlots.Add(slot);
+            }
+        }
 
-                if (invSlot.ItemID == itemID)
+        public void TestAdd()
+        {
+            Add(1, 10);
+            Add(2, 1);
+        }
+
+        public void TestRemove()
+        {
+            Remove(1, 7);//remove seven apples
+            Remove(2, 1);//and one slime
+            //apple is item type 1
+            //slime is item type 2
+        }
+
+        /// <summary>
+        /// Adds items to the inventory
+        /// </summary>
+        /// <param name="itemID">The type of item to add to the inventory</param>
+        /// <param name="quantity">The amount of the item to add to the inventory</param>
+        public void Add(int itemID, int quantity)
+        {
+            //Overflow item amount e.g., if 20 is tried to add to a stack of 15 the excess would be 5
+            int excess = 0;
+
+            if (m_EmptySlots.Count > 0)
+            {
+                //checks slots which are already populated with that item type
+                foreach (GameObject slot in m_FilledSlots)
                 {
-                    slot.transform.SetAsLastSibling();
-                    excess = invSlot.AddItems(quantity);
+                    InventorySlot invSlot = slot.GetComponent<InventorySlot>();
 
-                    if (excess <= 0)
+                    if (invSlot.ItemID == itemID)//checks slots of the item type to get added
                     {
-                        return;
+                        slot.transform.SetAsLastSibling();//moves the new slot to the end of the menu
+                    
+                        //updates the item stack and returns any overflow of items which couldn't be added to the stack
+                        excess = invSlot.AddItems(quantity);
+
+                        //if there are no items left needing population - end the loop
+                        if (excess <= 0)
+                        {
+                            return;
+                        }
+                        //if there are items needing population, continue looping and make a new stack
                     }
+                }
+            
+                //If this is a new item, and we have slots left to fill
+                if (m_FilledSlots.Count < m_CurrentSlots)
+                {
+                    //Remove an empty slot from our empty slot list
+                    GameObject slot = m_EmptySlots[0];
+                    m_EmptySlots.RemoveAt(0);
+
+                    slot.GetComponent<InventorySlot>().InitItemSlot(GetItemByID(itemID), excess > 0 ? excess : quantity);
+                    slot.transform.SetParent(m_FilledSlotContainer.transform);
+
+                    //Puts the filled slot into the filled slot list instead thus, adding the item to it
+                    m_FilledSlots.Add(slot);
+                }
+                else
+                {
+                    /*
+                        TODO add scroll functionality here:
+                        would be an if statement like if(current slots > page slots)
+                        move the screen down every time it is over page slots
+                        this whole thing would need to be in if current slots < max slots
+                        then return if current slots == max slots
+                    */
                 }
             }
         }
 
-        if (m_EmptySlots.Count > 0 && m_FilledSlots.Count < m_CurrentSlots)
+        /// <summary>
+        /// Removes items from the inventory
+        /// </summary>
+        /// <param name="itemID">The type of item to remove from the inventory</param>
+        /// <param name="quantity">The amount of the item to remove from the inventory</param>
+        public void Remove(int itemID, int quantity)
         {
-            GameObject slot = m_EmptySlots[0];
-            m_EmptySlots.RemoveAt(0);
+            List<GameObject> slotsToRemove = new List<GameObject>();
 
-            slot.GetComponent<InventorySlot>().InitItemSlot(GetItemByID(itemID), excess > 0 ? excess : quantity);
-            slot.transform.SetParent(m_FilledSlotContainer.transform);
-
-            m_FilledSlots.Add(slot);
-        }
-    }
-
-    public void Remove(int itemID, int quantity)
-    {
-        List<GameObject> slotsToRemove = new List<GameObject>();
-
-        if (m_FilledSlots.Count > 0)
-        {
-            int excess = quantity;
-
+            //Validates that there are enough items left in the array to be removed
+            if (m_FilledSlots.Count <= 0)
+            {
+                Debug.LogWarning("Insufficient items to remove");
+                return;
+            }
+    
+            //Checks slots which are already populated with that item type
             foreach (GameObject slot in m_FilledSlots)
             {
                 InventorySlot invSlot = slot.GetComponent<InventorySlot>();
 
-                if (invSlot.ItemID == itemID && excess > 0)
+                
+                //checks slots of the same item type to be removed
+                if (invSlot.ItemID == itemID && quantity > 0)
                 {
-                    excess = invSlot.RemoveItems(excess);
+                    quantity = invSlot.RemoveItems(quantity);
 
-                    if (excess >= 0)
+                    if (quantity >= 0)
                     {
+                        //Queues them up for removal if they’re going to need to be removed 
                         slotsToRemove.Add(slot);
 
-                        if (m_FilledSlots.Count == 0 && excess > 0)
+                        if (m_FilledSlots.Count == 0 && quantity > 0)
                         {
                             Debug.LogWarning("Insufficient items to remove");
                         }
                     }
                 }
             }
-
-            foreach (GameObject slot in slotsToRemove)
+        
+            //moves every slot to remove into the empty slot array
+            //this is in a separate foreach so the collection isn't being edited whilst you're iterating through it
+            foreach (GameObject slot in slotsToRemove)//in slotsToRemove as opposed to filled slots like before
             {
                 slot.transform.SetParent(m_EmptySlotContainer.transform);
-                m_FilledSlots.Remove(slot);
+                m_FilledSlots.Remove(slot);//can be edited now as another collection is being used to iterate
                 m_EmptySlots.Add(slot);
             }
-
-            return;
-
         }
 
-        Debug.LogWarning("Insufficient items to remove");
-    }
-
-    private ItemSO GetItemByID(int id)
-    {
-        foreach (ItemSO item in m_ItemDatabase)
+        private ItemSO GetItemByID(int id)
         {
-            if (item.ItemID == id)
+            foreach (ItemSO item in m_ItemDatabase)
             {
-                return item;
+                if (item.ItemID == id)
+                {
+                    return item;
+                }
             }
+            Debug.LogError("Item ID " + id + " not found in database");
+            return null;
         }
-        Debug.LogError("Item ID " + id + " not found in database");
-        return null;
     }
 }
--- Assets/Tutorials/Inventory/Scripts/InventorySlot.cs
+++ Assets/Tutorials/Inventory/Scripts/InventorySlot.cs
@@ -27,6 +27,11 @@ public class InventorySlot : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Item slots must be initialized/filled before use.
+    /// </summary>
+    /// <param name="item">Item to be initialised</param>
+    /// <param name="quantity">The amount of the item to be initialised</param>
     public void InitItemSlot(ItemSO item, int quantity)
     {
         int excess = Math.Max(-item.MaxStack + quantity, 0);
@@ -65,41 +70,41 @@ public class InventorySlot : MonoBehaviour
 
     public int AddItems(int quantity)
     {
-        if (!ItemInitialized()) return 0;
+        if (!ItemInitialized()) return 0;//validity check
 
-        int newQuantity = m_ItemQuantity + quantity;
+        int newQuantity = m_ItemQuantity + quantity;//updates quantity
 
-        if (newQuantity >= m_Item.MaxStack)
+        if (newQuantity >= m_Item.MaxStack)//if the stack should be full
         {
-            m_ItemQuantity = m_Item.MaxStack;
-            m_QuantityText.text = m_ItemQuantity.ToString();
+            m_ItemQuantity = m_Item.MaxStack;//fill the stack
+            m_QuantityText.text = m_ItemQuantity.ToString();//updates the UI
 
-            return newQuantity - m_Item.MaxStack;
+            return newQuantity - m_Item.MaxStack;//returns the amount of items which couldn't be added to the current stack 
         }
 
-        m_ItemQuantity = newQuantity;
-        m_QuantityText.text = newQuantity.ToString();
-        return 0;
+        m_ItemQuantity = newQuantity;//updates the quantity
+        m_QuantityText.text = newQuantity.ToString();//updates the UI
+        return 0;//no overflow so returns zero
     }
 
     public int RemoveItems(int quantity)
     {
-        if (!ItemInitialized()) return 0;
+        if (!ItemInitialized()) return 0;//another validity check
 
-        int newQuantity = m_ItemQuantity - quantity;
+        int newQuantity = m_ItemQuantity - quantity;//performs the item removal calc
 
-        if (newQuantity <= 0)
+        if (newQuantity <= 0)//if enough of the item has been removed
         {
-            ClearItemSlot();
+            ClearItemSlot();//clear the slot
 
-            m_ItemQuantity = 0;
-
-            return newQuantity < 0 ? -newQuantity : 0;
+            m_ItemQuantity = 0;//clear the quantity on that item
+            
+            return newQuantity < 0 ? -newQuantity : 0;//-newQuantity would return positive if newQuantity is negative 
         }
 
-        m_ItemQuantity = newQuantity;
-        m_QuantityText.text = newQuantity.ToString();
-        return -1;
+        m_ItemQuantity = newQuantity;//sets the item quantity if it is positive 
+        m_QuantityText.text = newQuantity.ToString();//updates the UI to reflect the new quantity
+        return -1;//returns -1 to signify the quantity was updated to a positive successfully  
     }
 
     public void ClearItemSlot()
